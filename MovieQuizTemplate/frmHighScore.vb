290494283a26eb603a181dd19af85909b3b7c687
e.chancusi@wlc.ac.uk <e.chancusi@wlc.ac.uk>
2023-04-17 13:51:32 +0100

    Updated again button in the high score form

--- MovieQuizTemplate/frmHighScore.vb
+++ MovieQuizTemplate/frmHighScore.vb
@@ -1,3 +1,6 @@
 ﻿Public Class frmHighScore
-
+    Private Sub btnAgain_Click(sender As Object, e As EventArgs) Handles btnAgain.Click
+        frmMain.Show()
+        Me.Hide()
+    End Sub
 End Class
