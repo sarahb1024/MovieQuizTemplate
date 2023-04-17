7009bb3de5d253ba883df7b30aa34287a2a95c1e
Sarah Benjamin <s.benjamin@wlc.ac.uk>
2023-04-17 11:51:49 +0100

    Changed title spelling.  Changed named controls on buttons.

--- MovieQuizTemplate/frmQuestion1.vb
+++ MovieQuizTemplate/frmQuestion1.vb
@@ -6,4 +6,8 @@
     Private Sub RadioButton1_CheckedChanged_1(sender As Object, e As EventArgs) Handles btnAnswer4.CheckedChanged
 
     End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+
+    End Sub
 End Class
