495825c11513adbfa1d2877e3e03ce713497b8c4
Sarah Benjamin <s.benjamin@wlc.ac.uk>
2023-04-17 14:04:30 +0100

    Edit Quit Button code on high score form.

--- MovieQuizTemplate/frmHighScore.vb
+++ MovieQuizTemplate/frmHighScore.vb
@@ -4,11 +4,11 @@
             lblMessage.Text = "Sorry Your Score Was Not Good Enough"
         Else
             lblMessage.Text = "Congratulations Your Score Will Be Added To The Hall Of Fame"
-1StHighScore
+1StHighScore.Items.Add(playerName & vbTab & vbTab & playerScore)
         End If
     End Sub
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
-
+        Application.Exit()
     End Sub
 
     Private Sub btnAgain_Click(sender As Object, e As EventArgs) Handles btnAgain.Click
