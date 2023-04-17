24779f9bfe03d550b6ce2d3dec2446e776d8d0b3
Sarah Benjamin <s.benjamin@wlc.ac.uk>
2023-04-17 11:41:44 +0100

    Edited radio buttons.  Changed named controls.

--- MovieQuizTemplate/frmQuestion1.vb
+++ MovieQuizTemplate/frmQuestion1.vb
@@ -1,3 +1,9 @@
 ﻿Public Class frmQuestion1
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles btnAnswer3.CheckedChanged
 
+    End Sub
+
+    Private Sub RadioButton1_CheckedChanged_1(sender As Object, e As EventArgs) Handles btnAnswer4.CheckedChanged
+
+    End Sub
 End Class
