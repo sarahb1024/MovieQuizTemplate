eaebae92ea90f4719063f682e82149ea7bd52759
Sarah Benjamin <s.benjamin@wlc.ac.uk>
2023-04-17 13:40:42 +0100

    Added code to buttons on main form.

--- MovieQuizTemplate/frmMain.vb
+++ MovieQuizTemplate/frmMain.vb
@@ -12,6 +12,13 @@
     End Sub
 
     Private Sub btnStart_Click(sender As Object, e As EventArgs) Handles btnStart.Click
+        playerScore = 0
+        playerName = txt.Name.Text
+        frmQuestion1.Show()
+        Me.Hide()
+    End Sub
 
+    Private Sub btnQuit_Click(sender As Object, e As EventArgs) Handles btnQuit.Click
+        Application.Exit()
     End Sub
 End Class
